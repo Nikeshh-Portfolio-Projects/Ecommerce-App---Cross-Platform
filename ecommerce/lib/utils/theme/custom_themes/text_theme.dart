e3d5397605bfe733f669dfc8fcbcd349b1f70f0c
Nikeshh Vijayabaskaran <nikeshhbaskaran01@gmail.com>
2024-02-28 17:22:22 -0500

    Added theme data

---
+++ ecommerce/lib/utils/theme/custom_themes/text_theme.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class TTextTheme {
+  TTextTheme._();
+
+  static TextTheme lightTextTheme = TextTheme(
+    headlineLarge: const TextStyle().copyWith(fontSize: 32.0, fontWeight: FontWeight.bold, color: Colors.black),
+    headlineMedium: const TextStyle().copyWith(fontSize: 24.0, fontWeight: FontWeight.w600, color: Colors.black)
+  );
+  static TextTheme darkTextTheme = TextTheme(
+    headlineLarge: const TextStyle().copyWith(fontSize: 32.0, fontWeight: FontWeight.bold, color: Colors.white),
+    headlineMedium: const TextStyle().copyWith(fontSize: 24.0, fontWeight: FontWeight.w600, color: Colors.white)
+  );
+}
