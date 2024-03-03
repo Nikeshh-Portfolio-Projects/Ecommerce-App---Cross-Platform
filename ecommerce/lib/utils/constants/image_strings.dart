8ec70de2c100c37f12e923d9616edf2ad2a2cde6
Nikeshh Vijayabaskaran <nikeshhbaskaran01@gmail.com>
2024-03-02 23:36:30 -0500

    Utility functions and Helpers

---
+++ ecommerce/lib/utils/constants/image_strings.dart
@@ -0,0 +1,9 @@
+class TImages {
+  // App Logos
+  static const String darkAppLogo = "assets/logos/splash-logo-black.png";
+  static const String lightAppLogo = "assets/logos/splash-logo-white.png";
+
+  // Social Logos
+  static const String google = "assets/logos/google-icon.png";
+  static const String facebook = "assets/logos/facebook-icon.png";
+}
---
+++ ecommerce/lib/utils/constants/text_strings.dart
@@ -0,0 +1,16 @@
+class TTexts {
+  // Global Texts
+
+  // OnBoarding Texts
+  static const String onBoardingTitle1 = "Choose your product";
+  static const String onBoardingTitle2 = "Select Payment Method";
+  static const String onBoardingTitle3 = "Deliver at your door step";
+
+  static const String onBoardingSubtitle1 = "Welcome to a World of Limitless Choices - Your Perfect Product Awaits!";
+  static const String onBoardingSubtitle2 = "For Seamless Transactions, Choose Your Payment Path - Your Convenience, Our Priority!";
+  static const String onBoardingSubtitle3 = "From Our Doorstep to Yours - Swift, Secure, and Contactless Delivery!";
+
+  // Home
+  static const String homeAppbarTitle = "Good day for shopping";
+  static const String homeAppbarSubTitle = "Nikeshh Vijayabaskaran";
+}
