defa4175f45cef6569f4808735d3492928d5a3d9
Nikeshh <nikeshhbaskaran01@gmail.com>
2024-06-08 18:25:41 -0400

    Removed old version

--- ecommerce/lib/main.dart
+++
@@ -1,6 +0,0 @@
-import 'package:ecommerce/app.dart';
-import 'package:flutter/material.dart';
-
-void main() {
-  runApp(const MyApp());
-}
--- ecommerce/lib/utils/constants/api_constants.dart
+++
@@ -1,3 +0,0 @@
-class ApiConstants {
-  static const String tSecretAPIKey = "";
-}
